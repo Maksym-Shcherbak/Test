a5f0fa412f236cb7e43355008cb69607c4de2c0c
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-07-07 01:09:35 +0300

    complete task

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
+let textFromUser = prompt("").replace(/[^a-zа-яіїєґ ]/gi, "");
+console.log(textFromUser);
+const firstUniqueChar = findUniqueSymbol(textFromUser);
+alert(`First unique symbol is ${firstUniqueChar}`);
+
 function findUniqueSymbol(text) {
-  let arrFromText = text.split(" ");
-  let arrFromWord = [];
-  let newArr = [];
-  let arrOfUniqueLetter = [];
   let firstUniqueLetter;
+  let noRepeatLetter = [];
+  let repeatLetter = [];
+
+  let arrFromText = text.split(" ");
+
   for (let word of arrFromText) {
-    arrFromWord = word.split("");
-    newArr.push(arrFromWord);
-  }
-
-  for (let arrOfLetter of newArr) {
-    firstUniqueLetter = arrOfLetter[0];
-
-    arrOfLetter.shift(firstUniqueLetter);
-    // console.log(arrOfLetter[0]);
-    console.log(arrOfLetter);
-    for (let i = 0; i < arrOfLetter.length; i += 1) {
-      if (arrOfLetter.includes(firstUniqueLetter)) {
-        firstUniqueLetter = arrOfLetter[i];
-        arrOfLetter.shift(i);
-        i -= 1;
+    let arrFromWord = word.split("");
+    firstUniqueLetter = arrFromWord[0];
+    for (let i = 0; i < arrFromWord.length; i += 1) {
+      if (
+        !arrFromWord.includes(arrFromWord[i], i + 1) &&
+        !repeatLetter.includes(arrFromWord[i])
+      ) {
+        noRepeatLetter.push(arrFromWord[i]);
+        break;
+      } else {
+        repeatLetter.push(arrFromWord[i]);
       }
-
-      console.log(arrOfLetter.includes(firstUniqueLetter));
     }
-    console.log(firstUniqueLetter);
-    arrOfUniqueLetter.push(firstUniqueLetter);
+    repeatLetter = [];
   }
-  //   firstUniqueLetter = arrFromWord[0];
-  //   console.log(arrFromWord[0]);
-  //   arrFromWord.shift(firstUniqueLetter);
-  //   for (let i = 0; i < arrFromWord.length; i += 1) {
-  //     if (arrFromWord.includes(firstUniqueLetter)) {
-  //       firstUniqueLetter = arrFromWord[i];
-  //       arrOfUniqueLetter.push(firstUniqueLetter);
-  //     }
-  //     arrFromWord.shift(i);
-  //     console.log(arrFromWord.includes(firstUniqueLetter));
-  //   }
-  console.log(arrFromText);
-  console.log(newArr);
-  console.log(arrOfUniqueLetter);
 
-  return arrOfUniqueLetter;
+  for (let i = 0; i < noRepeatLetter.length; i += 1) {
+    let uniLetter = noRepeatLetter[i];
+    if (
+      !noRepeatLetter.includes(uniLetter, i + 1) &&
+      !repeatLetter.includes(uniLetter)
+    ) {
+      return uniLetter;
+    } else {
+      repeatLetter.push(uniLetter);
+    }
+  }
+  return null;
 }
-console.log(
-  findUniqueSymbol(
-    "Дізнаємося середній рейтинг усієї нашої колекції. Для цього, додамо усі рейтинги, після чого поділимо це значення на кількість книг в колекції."
-  )
-);
