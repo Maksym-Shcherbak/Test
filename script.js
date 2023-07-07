a3e02d28ef108f88f1f1e45faacd49158a75da0b
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-07-07 21:49:23 +0300

    optimize

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+// оголуємо змінну і записуємо у неї введений текст, одразу видаляємо усі символи окрім букв і пробілів
 let textFromUser = prompt("").replace(/[^a-zа-яіїєґ ]/gi, "");
 console.log(textFromUser);
+// записуємо у змінну результат виконання функції
 const firstUniqueChar = findUniqueSymbol(textFromUser);
+// виводимо результат виконання функції користувачу
+if (firstUniqueChar === null) {
+  alert("Canceled. Please, refresh page.");
+}
 alert(`First unique symbol is ${firstUniqueChar}`);
 location.reload();
 
 function findUniqueSymbol(text) {
-  let firstUniqueLetter;
+  // оголушуємо змінні, в яких будуть зберігатися унікальні символи та символи,які повторюються
   let noRepeatLetter = [];
   let repeatLetter = [];
-
+  // робимо з отриманого рядка масив
   let arrFromText = text.split(" ");
-
+  // перебираємо кожне слово масиву
   for (let word of arrFromText) {
+    //ролбимо з кожного слова масив з літер та перебираємо його за допомогою циклу
     let arrFromWord = word.split("");
-    firstUniqueLetter = arrFromWord[0];
     for (let i = 0; i < arrFromWord.length; i += 1) {
+      // перевіряємо чи повторюється символ у слові та чи є він у масиві символів, що повторюються, якщо ні, то додаємо його у масив унікальних символів,
+      // якщо так, то додаємо у масив символів, що повторюються
       if (
         !arrFromWord.includes(arrFromWord[i], i + 1) &&
         !repeatLetter.includes(arrFromWord[i])
@@ -25,18 +33,14 @@ function findUniqueSymbol(text) {
         repeatLetter.push(arrFromWord[i]);
       }
     }
+    // у кінці циклу обнуляємо масив символів, що повторюються
     repeatLetter = [];
   }
-
+  // перебираємо масив з унікальних символів i шукаємо перший неповторюваний символ
   for (let i = 0; i < noRepeatLetter.length; i += 1) {
     let uniLetter = noRepeatLetter[i];
-    if (
-      !noRepeatLetter.includes(uniLetter, i + 1) &&
-      !repeatLetter.includes(uniLetter)
-    ) {
+    if (!noRepeatLetter.includes(uniLetter, i + 1)) {
       return uniLetter;
-    } else {
-      repeatLetter.push(uniLetter);
     }
   }
   return null;
