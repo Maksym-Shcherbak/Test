ed0e440b2cbafda5189301b037ffe478bfa3ffdb
Maksym-Shcherbak <max.scherbak93@gmail.com>
2023-07-21 09:05:08 +0300

    update script 2

--- script_second.js
+++ script_second.js
@@ -7,32 +7,23 @@ if (textFromUser === null) {
   alert("Canceled. Please, enter refresh page.");
 } else {
   const firstUniqueChar = (text) => {
-    // оголушуємо масив, в якому будуть зберігатися унікальні символи
-    let noRepeatChar = [];
-    // видаляємо усі символи окрім букв, робимо з отриманого рядка масив із слів
+    //видаляємо усі символи окрім букв і робимо масив із слів
     let arrFromText = text.replace(/[^a-zа-яіїєґ]/gi, " ").split(" ");
-    console.log(arrFromText);
-    // перебираємо кожне слово масиву
-    for (let word of arrFromText) {
-      if (word === " ") {
-        continue; // пропускаємо пробіли у масиві
-      }
-      //ролбимо з кожного слова масив з літер та шукаємо перший унікальний символ за допомогою методу find
-      let arrFromWord = word.split("");
-      // додаємо у масив унікальний символ
-      noRepeatChar.push(
-        [...arrFromWord].find((n, i, a) => a.indexOf(n) === a.lastIndexOf(n))
-      );
-    }
-    // знаходимо у масиві унікальних символів перший унікальний символ
-    const uniChar = [...noRepeatChar].find(
-      (n, i, a) => a.indexOf(n) === a.lastIndexOf(n)
-    );
-    console.log(uniChar);
-    if (uniChar) {
-      return alert(`First unique symbol is ${uniChar}`);
-    }
-    return alert(`First unique symbol is not find`);
+    //створюємо змінну для збереження унікального символа
+    const uniChar = arrFromText
+      //використовуючи метод reduce, створюємо масив з унікальних символів у кожному слові
+      .reduce((noRepeatChar, word) => {
+        console.log(word);
+        noRepeatChar.push(
+          word.split("").find((n, i, a) => a.indexOf(n) === a.lastIndexOf(n))
+        );
+        return noRepeatChar;
+      }, [])
+      //знаходимо унікальний символ
+      .find((n, i, a) => a.indexOf(n) === a.lastIndexOf(n));
+    uniChar
+      ? alert(`First unique symbol is ${uniChar}`)
+      : alert(`First unique symbol is not find`);
   };
   firstUniqueChar(textFromUser);
 }
